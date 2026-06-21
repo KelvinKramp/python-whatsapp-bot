9331c22a3df3adfbedda35ed00f69ca569bdc2e1
Kelvin Kramp <k.h.kramp@gmail.com>
2026-06-21 22:40:45 +0200

    add debug

--- run.py
+++ run.py
@@ -9,4 +9,5 @@ app = create_app()
 if __name__ == "__main__":
     logging.info("Flask app started")
     port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)
+    debug = os.environ.get("FLASK_DEBUG", "1").lower() not in ("0", "false", "no")
+    app.run(host="0.0.0.0", port=port, debug=debug, use_reloader=debug)
